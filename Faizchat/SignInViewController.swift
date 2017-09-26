adc97cf13ef7417d499a2c4a5a6470929269b1f3
Mohammed Faiz <mfaizudd@neiu.edu>
2017-09-26 11:30:37 -0500

    configured with Firebase.. Sign In Screen created..

---
+++ Faizchat/SignInViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignInViewController.swift
+//  Faizchat
+//
+//  Created by Mohammed Faizuddin on 9/26/17.
+//  Copyright © 2017 Faiz Tech. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    @IBAction func SignTapped(_ sender: Any) {
+    }
+    
+}
+
